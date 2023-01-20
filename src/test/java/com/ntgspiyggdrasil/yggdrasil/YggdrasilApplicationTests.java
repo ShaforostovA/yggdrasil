1bfdd8f40a42b8811f367866300ec75e5a56e446
Yama Senpai <manax.2001@mail.ru>
2023-01-20 22:16:33 +0500

    Creating a Yggdrasil project.

---
+++ src/test/java/com/ntgspiyggdrasil/yggdrasil/YggdrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntgspiyggdrasil.yggdrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YggdrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
