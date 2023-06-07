15fa0e7a6d1de04389078526b0365d99c77b7774
Yama Senpai <manax.2001@mail.ru>
2023-06-07 15:17:08 +0500

    Docker

--- src/main/java/com/ntgspiyggdrasil/yggdrasil/payload/response/FacultyModel.java
+++ src/main/java/com/ntgspiyggdrasil/yggdrasil/payload/response/FacultyModel.java
@@ -5,6 +5,7 @@ import com.ntgspiyggdrasil.yggdrasil.models.Faculty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
