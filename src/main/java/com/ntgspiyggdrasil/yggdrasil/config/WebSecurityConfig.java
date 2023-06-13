961cee5bf5979b3ee74be649bb0709b749350f2b
Yama Senpai <manax.2001@mail.ru>
2023-06-13 14:20:24 +0500

    cors off

--- src/main/java/com/ntgspiyggdrasil/yggdrasil/config/WebSecurityConfig.java
+++ src/main/java/com/ntgspiyggdrasil/yggdrasil/config/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().requestMatchers("/api/v1/auth/**").permitAll()
