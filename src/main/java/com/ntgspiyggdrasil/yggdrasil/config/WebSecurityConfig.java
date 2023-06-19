4ab47937f76435b5701756398b597a78acfde3c0
Yama Senpai <manax.2001@mail.ru>
2023-06-19 23:12:31 +0500

    pleas, work it

--- src/main/java/com/ntgspiyggdrasil/yggdrasil/config/WebSecurityConfig.java
+++ src/main/java/com/ntgspiyggdrasil/yggdrasil/config/WebSecurityConfig.java
@@ -55,10 +55,11 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().requestMatchers("/api/v1/auth/**").permitAll()
+                                    .requestMatchers("/**").permitAll()
                 .anyRequest().authenticated();
 
         http.authenticationProvider(authenticationProvider());
--- src/main/java/com/ntgspiyggdrasil/yggdrasil/controllers/KeyWordController.java
+++ src/main/java/com/ntgspiyggdrasil/yggdrasil/controllers/KeyWordController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Set;
 
-//@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/keywords")
 @AllArgsConstructor
